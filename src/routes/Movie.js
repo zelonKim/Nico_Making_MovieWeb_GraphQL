57241d790acf6d461f4aa32a0b8bb37121e16812
zelonKim <ksz18601@gmail.com>
2023-09-21 14:41:50 +0900

    ApolloClient & useQuery hook

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,56 @@
+import { gql, useQuery } from "@apollo/client"
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+    query getMovie($movieId: String!) {
+        movie(id: $movieId) {
+            id
+            title
+            rating
+            medium_cover_image
+            isLiked @client
+        }
+    }
+`
+
 export default function Movie() {
-    return <div>Movie Detail</div>
+    const { id } = useParams();
+
+    const { data, loading, client: {cache} } = useQuery(GET_MOVIE, {
+        variables: {
+            movieId: id,
+        }
+    })
+
+    if(loading) {
+        return <h1> Loading... </h1>
+    }
+
+     const onClick = () => {
+        cache.writeFragment({
+            id: `Movie:${id}`,
+
+            fragment: gql`
+                fragment MovieFragment on Movie {
+                    isLiked
+                }
+            `,
+
+            data: {
+                isLiked: !data.movie.isLiked,
+            },
+        }) 
+    }
+
+    return (
+    <div>
+        {data.movie.title}  /  ★ {data.movie.rating}
+        <div>
+            <img src={data.movie.medium_cover_image}></img>
+        </div>
+        <div>
+            <button onClike={onClick}>{data.movie.isLiked ? "Unlike" : "Like"}</button>
+        </div>
+    </div>
+    )
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,34 @@
+import { gql, useQuery } from "@apollo/client"
+import { Link } from "react-router-dom"
+
+const ALL_MOVIES = gql`
+     query getMovies {
+        allMovies {
+            id
+            title
+            medium_cover_image
+        }
+    }
+`
+
 export default function Movies() {
-    return <div>List of Movies</div>
+    const { data, loading, error } = useQuery(ALL_MOVIES)
+
+    if(loading) {
+        return <h1>Loading...</h1>
+    }
+    if(error) {
+        return <h1>Error...</h1>
+    }
+    return (
+        <ul> 
+            <h1>Movies</h1>
+            {data.allMovies.map(movie => 
+            <li key={movie.id}>
+                <Link to={`/movies/${movie.id}`}>
+                    <img src={movie.medium_cover_image}></img>
+                </Link>
+            </li>
+    )} </ul>
+    )
 }
